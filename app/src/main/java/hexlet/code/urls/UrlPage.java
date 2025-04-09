2d92e038352c40e3787065474ee893c163f786e6
denis <neksus87@gmail.com>
2025-04-09 13:52:18 +0700

    Project 72 step 7

--- app/src/main/java/hexlet/code/urls/UrlPage.java
+++ app/src/main/java/hexlet/code/urls/UrlPage.java
@@ -1,12 +1,16 @@
-package hexlet.code.dto.urls;
+package hexlet.code.urls;
 
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
 public class UrlPage extends BasePage {
     private Url url;
+    private List<UrlCheck> checks;
 }
--- app/src/main/java/hexlet/code/urls/UrlsPage.java
+++ app/src/main/java/hexlet/code/urls/UrlsPage.java
@@ -1,13 +1,16 @@
-package hexlet.code.dto.urls;
+package hexlet.code.urls;
 
 import hexlet.code.dto.BasePage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Getter
 public class UrlsPage extends BasePage {
     private List<Url> urls;
+    private Map<Long, UrlCheck> latestChecksByUrlId;
 }
